330e3808bf577d0be1fb03bd1a023875bf7140d8
Afdal OULOUBE <aouloube@student.42.fr>
2015-11-30 19:12:50 +0100

    part1 fini

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: aouloube <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 11:07:09 by aouloube          #+#    #+#             */
-/*   Updated: 2015/11/28 13:47:52 by aouloube         ###   ########.fr       */
+/*   Updated: 2015/11/30 18:53:14 by aouloube         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	*ft_memalloc(size_t size)
 {
-	char	*str;
+	void	*m;
 
-	str = (char *)malloc(sizeof(char) * size);
-	if (str)
-		return (str);
-	else
+	if (size == 0)
 		return (NULL);
+	else
+		m = malloc(size);
+	return (m);
 }
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,156 +6,99 @@
 /*   By: aouloube <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 12:31:07 by aouloube          #+#    #+#             */
-/*   Updated: 2015/11/29 18:25:11 by aouloube         ###   ########.fr       */
+/*   Updated: 2015/11/30 18:54:44 by aouloube         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strcount(char const	*s, char c)
+int		ft_countword(char const *s, char c)
 {
 	int		i;
-	int		count;
+	int		res;
 
+	res = 0;
 	i = 0;
-	count = 0;
 	while (s[i])
 	{
 		if (s[i] != c)
-			count++;
-		i++;
+		{
+			while (s[i] && s[i] != c)
+				i++;
+			res++;
+		}
+		if (s[i] == c)
+			i++;
 	}
-	return (count);
-}
-
-int		ft_countc(char const *s, char c)
-{
-	int i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (s[i])
-	{
-		if	(s[i] == c)
-			count++;
-		i++;
-	}
-
-	return (count);
-}
-
-
-char    *ft_strtrimchar(char const *s, char c)
-{
-	unsigned int	 start;
-	unsigned int	end;
-	char			*res;
-
-	if (!s || s[0] == '\0')
-		return (ft_strnew(1));
-	start = 0;
-	while (s[start] == c)
-		start++;
-	end = ft_strlen(s) - 1;
-	while ((s[end] == c) && end > start)
-		end--;
-	res = ft_strsub(s, start, end - start + 1);
 	return (res);
 }
 
-int		*ft_strintab(char const *str, char c)
+int		ft_intab(char const *s, char c, int *z)
 {
 	int		i;
-	int		y;
-	int		*intab;
-	int		size;
 
 	i = 0;
-	y = 0;
-	size = 0;
-	intab = (int *)malloc(sizeof(int) * ft_countc(str, c));
-	while (str[i])
+	if (s[*z] == c)
 	{
-		if(str[i] == c)
-		{
-			y++;
-			size = 0;
-		}
-		else
-		{
-			size++;
-			intab[y - 1] = size;
-		}
-		i++;
+		while (s[*z] == c)
+			(*z)++;
 	}
-	return (intab);
+	while (s[*z] != c && s[*z])
+	{
+		i++;
+		(*z)++;
+	}
+	return (i);
 }
 
-char	**ft_malloc2dm(char const *s, char c)
+char	**ft_malloc2dm(char **tmp, char const *s, char c)
 {
-	char	*str;
-	int		*intab;
-	char	**st;
-	int		size =  ft_countc(s, c);
 	int		i;
-	
-	str = ft_strtrimchar(s, c);
-	intab = ft_strintab(s, c);
-	st = (char **)malloc(sizeof(char *) * size + 1);
+	int		u;
+	int		z;
+
 	i = 0;
-	ft_putnbr(size + 1);
-	return (NULL);
-	while (size + 1)
+	u = 0;
+	z = 0;
+	while (s[i] != '\0')
 	{
-		st[i] = ft_strnew(intab[i]);
-		size--;
-		i++;
+		if (s[i] != c)
+		{
+			while (s[i] != c && s[i] != '\0')
+			{
+				tmp[u][z] = s[i];
+				i++;
+				z++;
+			}
+			z = 0;
+			u++;
+		}
+		if (s[i] == c)
+			i++;
 	}
-	return (st);
+	tmp[u] = 0;
+	return (tmp);
 }
 
-char	 **ft_strsplit(char const *s, char c){
+char	**ft_strsplit(char const *s, char c)
+{
 	char	**toto;
-	int		size;
 	int		i;
-	int		y;
 	int		x;
-	char	*s2;
+	int		y;
 
-	s2 = ft_strtrimchar(s, c);
-	size = ft_countc(s2, c);
-	toto = ft_malloc2dm(s2, c);
-	i = 0;
-	y = 0;
 	x = 0;
-	while (s2[i])
+	y = 0;
+	if (!s)
+		return (NULL);
+	i = ft_countword(s, c);
+	toto = (char **)malloc(sizeof(char*) * (i + 1));
+	if (!toto)
+		return (NULL);
+	while (i > x)
 	{
-		if(s2[i] == c && s2[i + 1] != c)
-		{
-			y++;
-			x = 0;
-		}
-		else
-		{	
-			toto[y][x] = s2[i];
-			x++;
-		}	
-		i++;
-		
+		toto[x] = (char *)malloc(sizeof(char) * (ft_intab(s, c, &y) + 1));
+		x++;
 	}
-	return (toto);
-}
-
-
-
-int main(int argc, char **argv)
-{
-	char	**fap;
-	if (argc == 3)
-	{
-		fap = ft_strsplit("***salut****!**", '*');
-		ft_putstr(fap[0]);
-	}
-	return (0);
+	return (ft_malloc2dm(toto, s, c));
 }
